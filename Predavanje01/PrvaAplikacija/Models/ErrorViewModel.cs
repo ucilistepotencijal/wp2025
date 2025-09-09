6ad5bee82cf8665b219a625d7f5c385240124b2b
ucilistepotencijal <144696093+ucilistepotencijal@users.noreply.github.com>
2025-09-09 07:57:48 +0200

    Added Predavanje01

---
+++ Predavanje01/PrvaAplikacija/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PrvaAplikacija.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
