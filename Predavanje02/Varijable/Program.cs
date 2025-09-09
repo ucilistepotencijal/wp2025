661dcff7f63b93d627c96d154383fb2546317890
Meri  Sumic <merisumic@gmail.com>
2025-09-09 19:22:13 +0200

    Added Predavanje02 solutions

---
+++ Predavanje02/Varijable/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//tekstualna varijabla
+string ime = "Marko";
+Console.WriteLine(ime);
+
+//numerička varijabla
+//integer - cijeli broj
+int godine = 25;
+Console.WriteLine(godine);
+
+int broj = Convert.ToInt32("123");
+
+Console.WriteLine(broj);
