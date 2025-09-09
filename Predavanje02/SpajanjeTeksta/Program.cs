4cab412c80b79abb5ee0b94ef97b01a9e60931e0
franjobalog-95 <franjo.balog@gmail.com>
2025-09-09 20:53:13 +0200

    Finished Predavanje02

---
+++ Predavanje02/SpajanjeTeksta/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+string ime = "Pero";
+string prezime = "Perić";
+string grad = "Osijek";
+
+
+//1. Konkatenacija (spajanje) pomoću + operatora
+Console.WriteLine(ime + " " + prezime);
+
+//2. Placeholders (mjesta za zamjenu)
+Console.WriteLine("{0} {1} {2}", ime, prezime, grad);
+
+
+string punoIme = string.Format("{0} {1} {2}", ime, prezime, grad);
+Console.WriteLine(punoIme);
+
+
+
+
