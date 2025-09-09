bf9051544152eeab1fd4ed87c4b1c515fcdf7b5e
franjobalog-95 <franjo.balog@gmail.com>
2025-09-09 19:22:01 +0200

    Added Predavanje02 solution

---
+++ Predavanje02/Varijable/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//tekstualna varijabla
+string ime = "Marko";
+Console.WriteLine(ime);
+
+//numerička varijabla
+int godine = 25;
+Console.WriteLine(godine);
+
+int broj = Convert.ToInt32("123");
+
+Console.WriteLine();
+
+
+
+
