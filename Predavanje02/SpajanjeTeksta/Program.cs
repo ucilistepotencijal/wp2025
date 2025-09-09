499471baa54c17c8a2755a842b6cf12a80f6b669
Meri  Sumic <merisumic@gmail.com>
2025-09-09 20:53:47 +0200

    Finished Predavanje02

---
+++ Predavanje02/SpajanjeTeksta/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+string ime = "Pero";
+string prezime = "Perić";
+
+//1. Konkatenacija (spajanje) pomoću + operatora
+Console.WriteLine(ime + " " + prezime);
+
+//2. Placeholderi (mjesta za zamjenu) - koristi se indeks varijable
+Console.WriteLine( "{0} {1}", ime, prezime);
+
+string punoIme = string.Format("{0} {1}", ime, prezime);
+Console.WriteLine(punoIme);
