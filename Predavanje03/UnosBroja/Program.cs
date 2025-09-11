cb446cdb355a42cbfb9a46063daff060f3ea40b4
ucilistepotencijal <144696093+ucilistepotencijal@users.noreply.github.com>
2025-09-11 19:30:08 +0200

    Added Predavanje03

---
+++ Predavanje03/UnosBroja/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace UnosBroja
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //deklaracija varijable
+            Int32 broj;
+
+            //inicijalizacija varijable
+            broj = 5;
+
+            //Eksplicitna pretvorba
+            int drugiBroj = Convert.ToInt32(12);
+            decimal broj3 = 13;
+            decimal broj4 = 15.5m;
+
+            //deciMal
+            //castanje
+            broj = (int)broj4;
+
+            Console.WriteLine(broj);
+            Console.WriteLine(drugiBroj);
+            Console.WriteLine(broj3);
+            Console.WriteLine(broj4);
+
+            Console.WriteLine("Unesi neki tekst: ");
+            string unos = Console.ReadLine();
+
+            Console.WriteLine("Unos: " + unos);
+
+            Console.WriteLine("Unesi neki broj:");
+            int uneseniBroj = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Unesi drugi broj:");
+            int uneseni_Broj_2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(uneseniBroj + uneseni_Broj_2);
+        }
+    }
+}
