694546f09a223d64816a6d5ce5f2137097fd30e4
ucilistepotencijal <144696093+ucilistepotencijal@users.noreply.github.com>
2025-09-15 20:55:09 +0200

    Finished predavanje 04

---
+++ Predavanje04/BrojVeciOd10/Program.cs
@@ -0,0 +1,30 @@
+﻿
+//Korisnik traži unos sa konzole koji sprema u cjelobrojnu varijablu. Napiši program koji ispisuje
+//"Broj je veći od 10" ako je, odnosno "Broj je manji ili jednak 10" ako nije veći od 10.
+
+Console.Write("unesite broj: ");
+int broj = int.Parse(Console.ReadLine());
+
+if (broj > 10)
+{
+    Console.WriteLine("Broj je veći od 10");
+}
+else
+{
+    Console.WriteLine("Broj je manji ili jednak 10");
+}
+
+//Prošireni if - digresija
+if (broj > 10)
+{
+    Console.WriteLine("Broj je veći od 10");
+}
+else if (broj == 10)
+{
+    Console.WriteLine("Broj je jednak 10");
+}
+else
+{
+    Console.WriteLine("Broj je manji od 10");
+}
+//Za uneseni broj treba provjeriti je li veći od 100, veći od 200 ili veći od 300
---
+++ Predavanje04/UgnijezdeniIf/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Unesi cijeli broj: ");
+int broj = int.Parse(Console.ReadLine());
+
+if (broj > 0)
+{
+    Console.WriteLine("Broj je pozitivan!");
+}
+else
+{
+    if (broj < 0)
+    {
+        Console.WriteLine("Broj je negativan!");
+    }
+    else
+    {
+        Console.WriteLine("Broj je nula!");
+    }
+}
