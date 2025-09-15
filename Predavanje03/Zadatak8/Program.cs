3dd7492a02d78c5e21e7913a35c8f4c79842ccc1
Meri  Sumic <merisumic@gmail.com>
2025-09-15 19:13:37 +0200

    Prepravljanje zadatka 8. (kalkulator za decimal, double i float)

--- Predavanje03/Zadatak8/Program.cs
+++ Predavanje03/Zadatak8/Program.cs
@@ -2,14 +2,41 @@
 int koristiš varijable tipa float, double i decimal (pronađi na koji način se zapisuje vrijednost
 decimal tipova varijabli). Koja je razlika u vrijednostima ispisanim kao rezultat operacije dijeljenja?*/
 
-decimal a = 6;
-decimal b = 4;
+decimal a = 2;
+decimal b = 3;
 
-decimal djeljenje = a / b;
-Console.WriteLine(djeljenje);
+decimal zbroj = a + b;
+decimal razlika = a - b;
+decimal umnozak = a * b;
+decimal kolicnik = a / b;
 
-float c = 6;
-float d = 4;
+Console.WriteLine("{0} + {1} = {2}", a, b, zbroj);
+Console.WriteLine("{0} - {1} = {2}", a, b, razlika);
+Console.WriteLine("{0} * {1} = {2}", a, b, umnozak);
+Console.WriteLine("{0} / {1} = {2}", a, b, kolicnik);
 
-float djeljenje2 = c / d;
-Console.WriteLine(djeljenje2);
+double a2 = 2;
+double b2 = 3;
+
+double zbroj2 = a2 + b2;
+double razlika2 = a2 - b2;
+double umnozak2 = a2 * b2;
+double kolicnik2 = a2 / b2;
+
+Console.WriteLine("{0} + {1} = {2}", a2, b2, zbroj2);
+Console.WriteLine("{0} - {1} = {2}", a2, b2, razlika2);
+Console.WriteLine("{0} * {1} = {2}", a2, b2, umnozak2);
+Console.WriteLine("{0} / {1} = {2}", a2, b2, kolicnik2);
+
+float a3 = 2;
+float b3 = 3;
+
+float zbroj3 = a3 + b3;
+float razlika3 = a3 - b3;
+float umnozak3 = a3 * b3;
+float kolicnik3 = a3 / b3;
+
+Console.WriteLine("{0} + {1} = {2}", a3, b3, zbroj3);
+Console.WriteLine("{0} - {1} = {2}", a3, b3, razlika3);
+Console.WriteLine("{0} * {1} = {2}", a3, b3, umnozak3);
+Console.WriteLine("{0} / {1} = {2}", a3, b3, kolicnik3);
