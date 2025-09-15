56051ef7784b5938f49ae45033e44ccf6a606559
domagoj <plecicdomagoj@gmail.com>
2025-09-15 20:55:31 +0200

    predavanje04

---
+++ predavanje04/ugjezdjeniIf/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.InputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("unesi cijeli broj: ");
+int broj = int.Parse(Console.ReadLine());
+
+if (broj > 0)
+{
+    Console.WriteLine("pozitivan je");
+}
+else
+{
+    if (broj < 0)
+    {
+        Console.WriteLine("negativan je");
+    }
+    else
+    {
+        Console.WriteLine("nula je");
+    }
+}
