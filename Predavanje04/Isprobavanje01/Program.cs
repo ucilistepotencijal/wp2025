d69c25df69cec0ce84c161c3c7982fece71cf762
franjobalog-95 <franjo.balog@gmail.com>
2025-09-16 20:18:27 +0200

    AddedIsprobavanje01

---
+++ Predavanje04/Isprobavanje01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.Design;
+
+Console.OutputEncoding  = System.Text.Encoding.UTF8;
+Console.InputEncoding = System.Text.Encoding.UTF8;
+
+
+Console.Write("Unesi cijeli broj: ");
+int number  = int.Parse(Console.ReadLine());
+
+if (number > 0)
+{
+    Console.WriteLine("Broj je pozitivan!");
+}
+else
+{
+    if (number < 0)
+    {
+        Console.WriteLine("Broj je negativan");
+    }
+    else
+    {
+        Console.WriteLine("Broj je nula!");
+    }
+}
