156480dd628551eb72cd017800a73c6dbe4c0d27
ucilistepotencijal <144696093+ucilistepotencijal@users.noreply.github.com>
2025-09-17 20:58:30 +0200

    Finished Predavanje05

---
+++ Predavanje05/Logicki_Ne/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Napiši naziv škole koju pohađaš: ");
+string nazivSkole = Console.ReadLine();
+
+if (nazivSkole != "Potencijal")
+{
+    Console.WriteLine("Upiši tečaj u Učilištu Potencijal!");
+}
+else
+{
+    Console.WriteLine("Bravo!");
+}
+
+//2. način
+
+//if (nazivskole == "potencijal")
+//{
+//    console.writeline("bravo!");
+//}
+//else
+//{
+//    console.writeline("upiši tečaj u učilištu potencijal!");
+//}
