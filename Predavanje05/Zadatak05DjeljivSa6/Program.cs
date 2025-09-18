9850e884147bc624503764b5422566b5e1ff1425
franjobalog-95 <franjo.balog@gmail.com>
2025-09-18 21:24:44 +0200

    Added Zadaća

---
+++ Predavanje05/Zadatak05DjeljivSa6/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Verujem li da ce se CAZZO ozeniti?");
+string prsten = "";
+
+while (prsten.ToLower() != "da" && prsten.ToLower() != "ne")
+{
+    Console.Write("Da li je prsten na ruci? ");
+    prsten = Console.ReadLine();
+
+    if (prsten.ToLower() == "da")
+    {
+        Console.WriteLine("Verujem!");
+    }
+    else if (prsten.ToLower() == "ne")
+    {
+        Console.WriteLine("Neverujem!");
+    }
+    else
+    {
+        Console.WriteLine("Molim odgovoriti sa DA ili NE!");
+    }
+
+}
+
+
+
