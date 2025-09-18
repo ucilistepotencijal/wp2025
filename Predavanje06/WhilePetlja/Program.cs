dcd446b4e370c8a26e17d2489fc1b46c741633c9
Meri  Sumic <merisumic@gmail.com>
2025-09-18 19:12:47 +0200

    Added Predavanje06

---
+++ Predavanje06/WhilePetlja/Program.cs
@@ -0,0 +1,29 @@
+﻿//1. Želim da korisnik upisuje svoje ime sve dok ne napiše kraj
+
+string ime = "";
+
+while (ime != "kraj")
+{
+    Console.Write("Upiši svoje ime: ");
+    ime = Console.ReadLine();
+
+    if (ime != "kraj")
+    {
+        Console.WriteLine("Dobar  dan {0}!", ime);
+    }
+    
+}
+
+//2. Do while petlja
+do
+{
+    Console.Write("Upiši svoje ime: ");
+    ime = Console.ReadLine();
+
+    if (ime != "kraj")
+    {
+        Console.WriteLine("Dobar  dan {0}!", ime);
+    }
+
+} while (ime != "kraj");
+Console.WriteLine();
