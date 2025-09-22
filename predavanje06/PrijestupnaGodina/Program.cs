b08e083898d4ffb4b5964341bf251fcefa6e2a87
domagoj <plecicdomagoj@gmail.com>
2025-09-22 18:30:27 +0200

    finish predavanje 06

---
+++ predavanje06/PrijestupnaGodina/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.Write("Unesite godinu: ");
+int godina = int.Parse(Console.ReadLine());
+
+if ((godina % 4 == 0 && godina % 100 != 0) || godina % 400 == 0)
+{
+    Console.WriteLine("Godina je prijestupna.");
+}
+else
+{
+    Console.WriteLine("Godina nije prijestupna.");
+}
