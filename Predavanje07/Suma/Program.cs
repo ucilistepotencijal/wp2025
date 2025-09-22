6c0181608725ad6a1574e2151b6aa2f29e33deeb
ucilistepotencijal <144696093+ucilistepotencijal@users.noreply.github.com>
2025-09-22 18:43:34 +0200

    Added Predavanje07

---
+++ Predavanje07/Suma/Program.cs
@@ -0,0 +1,12 @@
+﻿//Napiši program koji ispisuje sumu prvih 100 prirodnih brojeva.
+// 1 + 2 + 3 + 4 + 5 + .... + 95 + 96 + 97 + 98 + 99 + 100
+// n*(n+1)/2
+
+int suma = 0;
+for (int i = 1; i <= 100; i++)
+{
+    suma += i; // ili: suma = suma + i;
+}
+
+Console.WriteLine("Zbroj prvih 100 prirodnih brojeva je " + suma);
+
