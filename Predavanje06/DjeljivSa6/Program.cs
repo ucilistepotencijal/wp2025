6c1d5ad97481b29d2103a093c70e0547e0e3e79f
ucilistepotencijal <144696093+ucilistepotencijal@users.noreply.github.com>
2025-09-22 18:44:20 +0200

    Finished Predavanje06

--- Predavanje06/DjeljivSa6/Program.cs
+++ Predavanje06/DjeljivSa6/Program.cs
@@ -1,14 +1,13 @@
-﻿/*Napiši program u kojem se traži unos broja i ispisuje je li broj djeljiv sa 2 i sa 3 ili nije. Ako 
-jest, ispisuje se „Broj je djeljiv sa 6!“.
-*/
-Console.Write("Upiši bilo koji broj: ");
+﻿
+
+Console.Write("Unesite broj: ");
 int broj = int.Parse(Console.ReadLine());
 
-if (broj % 3 == 0 && broj % 2 == 0)
+if (broj % 2 == 0 && broj % 3 == 0)
 {
-    Console.WriteLine("Broj {0} je djeljiv sa 6!", broj);
+    Console.WriteLine("Broj je djeljiv sa 6!");
 }
 else
 {
-    Console.WriteLine("Broj {0} nije djeljiv sa 6!", broj);
+    Console.WriteLine("Broj nije djeljiv sa 6.");
 }
