735f89ee9a6782aab4b17f482dc1ca3562dca6bd
Meri  Sumic <merisumic@gmail.com>
2025-09-24 09:58:00 +0200

    Prepravljen zadatak (računanje prosjeka i ispis rezultata mi je bio unutar for petlje)

--- Predavanje07/ZavrsniUspjeh/Program.cs
+++ Predavanje07/ZavrsniUspjeh/Program.cs
@@ -3,31 +3,32 @@
 //program ispisuje završni uspjeh, inače ispisuje „Uspjeh je nedovoljan”. Za pogrešno unesenu
 //ocjenu treba zatražiƟ ponovni unos. */
 
-Console.Write("Unesi broj predmeta: ");
-int brojPredmeta = int.Parse(Console.ReadLine());
-int zbrojOcjena = 0;
-bool prosao = true;
 
-for (int i = 0; i < brojPredmeta; i++)
-{
-    bool ispravanUnos = false;
-    int ocjena;
-    do
+    Console.Write("Unesi broj predmeta: ");
+    int brojPredmeta = int.Parse(Console.ReadLine());
+    int zbrojOcjena = 0;
+    bool prosao = true;
+
+    for (int i = 0; i < brojPredmeta; i++)
     {
-        Console.Write("Unesi ocjenu: ");
-        ispravanUnos = int.TryParse(Console.ReadLine(), out ocjena);
-        if (ocjena < 1 || ocjena > 5)
+        bool ispravanUnos = false;
+        int ocjena;
+        do
         {
-            ispravanUnos = false;
+            Console.Write("Unesi ocjenu: ");
+            ispravanUnos = int.TryParse(Console.ReadLine(), out ocjena);
+            if (ocjena < 1 || ocjena > 5)
+            {
+                ispravanUnos = false;
+            }
+
+        } while (!ispravanUnos);
+        if (ocjena == 1)
+        {
+            prosao = false;
         }
-
-    } while (!ispravanUnos);
-    if (ocjena == 1)
-    {
-        prosao = false;
+        zbrojOcjena += ocjena;
     }
-    zbrojOcjena += ocjena;
-
     double prosjek = (double)zbrojOcjena / brojPredmeta;
 
     if (!prosao)
@@ -36,67 +37,17 @@ for (int i = 0; i < brojPredmeta; i++)
     }
     else if (prosjek >= 4.5)
     {
-        Console.WriteLine("Uspjeh je odličan");
+        Console.WriteLine("Uspjeh je odličan!");
     }
     else if (prosjek >= 3.5)
     {
-        Console.WriteLine("Uspjeh je vrlo dobar");
+        Console.WriteLine("Uspjeh je vrlo dobar!");
     }
     else if (prosjek >= 2.5)
     {
-        Console.WriteLine("Uspjeh je dobar");
+        Console.WriteLine("Uspjeh je dobar!");
     }
     else if (prosjek >= 1.5)
     {
-        Console.WriteLine("Uspjeh je dovoljan");
+        Console.WriteLine("Uspjeh je dovoljan!");
     }
-}
-
-//// ovo je s maina - vidit gdje mi je greška
-//Console.Write("Unesi broj predmeta: ");
-//int brojPredmeta = int.Parse(Console.ReadLine());
-//int zbrojOcjena = 0;
-//bool prosao = true;
-
-//for (int i = 0; i < brojPredmeta; i++)
-//{
-//    bool ispravanUnos = false;
-//    int ocjena;
-//    do
-//    {
-//        Console.Write("Unesi ocjenu: ");
-//        ispravanUnos = int.TryParse(Console.ReadLine(), out ocjena);
-//        if (ocjena < 1 || ocjena > 5)
-//        {
-//            ispravanUnos = false;
-//        }
-
-//    } while (!ispravanUnos);
-//    if (ocjena == 1)
-//    {
-//        prosao = false;
-//    }
-//    zbrojOcjena += ocjena;
-//}
-//double prosjek = (double)zbrojOcjena / brojPredmeta;
-
-//if (!prosao)
-//{
-//    Console.WriteLine("Uspjeh je nedovoljan!");
-//}
-//else if (prosjek >= 4.5)
-//{
-//    Console.WriteLine("Uspjeh je odličan!");
-//}
-//else if (prosjek >= 3.5)
-//{
-//    Console.WriteLine("Uspjeh je vrlo dobar!");
-//}
-//else if (prosjek >= 2.5)
-//{
-//    Console.WriteLine("Uspjeh je dobar!");
-//}
-//else if (prosjek >= 1.5)
-//{
-//    Console.WriteLine("Uspjeh je dovoljan!");
-//}
