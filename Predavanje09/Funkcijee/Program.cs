9623a352e00539c9c7c3d263af3d59461de8a605
ucilistepotencijal <144696093+ucilistepotencijal@users.noreply.github.com>
2025-09-25 19:10:52 +0200

    Added Predavanje09

---
+++ Predavanje09/Funkcijee/Program.cs
@@ -0,0 +1,20 @@
+﻿double rezultat = Kvadrat(5);
+Console.WriteLine(rezultat);
+
+int nekiBroj = UcitajCijeliBroj();
+Console.WriteLine(nekiBroj);
+
+partial class Program
+{
+    static double Kvadrat (double broj)
+    {
+        return broj * broj;
+    }
+
+    static int UcitajCijeliBroj()
+    {
+        Console.Write("Unesi cijeli broj: ");
+        int broj = int.Parse(Console.ReadLine());
+        return broj;
+    }
+}
