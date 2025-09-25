c6c13d97178cea3d39d743d221c7afdb6d7dad8d
franjobalog-95 <franjo.balog@gmail.com>
2025-09-25 14:38:15 +0200

    Added 13 Zadatak

---
+++ Predavanje07/Zadatak08Prosjek/Program.cs
@@ -0,0 +1,20 @@
+﻿decimal broj = 1;
+decimal razlika = 1;
+Console.Write("Unesi količinu prirodnih brojeva kojoj će se izračunati prosjek: ");
+decimal n = decimal.Parse(Console.ReadLine());
+
+
+
+
+for (decimal i = 1; i <= n; i++)
+{
+    Console.Write("Unesi prirodne brojeve za računanje prosjeka: ");
+    broj = decimal.Parse(Console.ReadLine());
+
+    broj += i;
+
+}
+
+    razlika = (decimal)broj / n;
+
+    Console.WriteLine("Prosjek je {0}!", razlika);
---
+++ Predavanje07/Zadatak13ParNepar/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+
+int parni = 0;
+int neparni = 0;
+int unos = 0;
+
+
+
+for (int i = 0; i <= unos; i++)
+{
+    Console.Write("Unesi prirodan broj: ");
+    unos = int.Parse(Console.ReadLine());
+
+
+    if (unos % 2 == 0)
+    {
+        parni += unos;
+    }
+    else if (unos % 2 != 0)
+    {
+        neparni += unos;
+    }
+    else if (unos == 0)
+    {
+        Console.WriteLine("Broj parnih brojeva u nizu je {0}", parni);
+        Console.WriteLine("Broj neparnih brojeva u nizu je {0}", neparni);
+    }
+}   
+
