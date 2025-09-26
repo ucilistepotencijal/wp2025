e5eeba9411d87f26a63d74c6fbe8bfdc57fd079e
franjobalog-95 <franjo.balog@gmail.com>
2025-09-26 19:19:17 +0200

    Added novo rjesenje ParNepar

--- Predavanje07/Zadatak13ParNepar/Program.cs
+++ Predavanje07/Zadatak13ParNepar/Program.cs
@@ -3,27 +3,21 @@
 
 int parni = 0;
 int neparni = 0;
-int unos = 0;
-
-
+int unos = 1;
 
 while (unos != 0)
 {
     Console.Write("Unesi prirodan broj: ");
     unos = int.Parse(Console.ReadLine());
 
-}
-for (int i = 0; i < unos; i++)
-{
     if (unos % 2 == 0)
     {
-        parni += unos;
+        parni++;
     }
     else
     {
-        neparni += unos;
+        neparni++;
     }
-
 }
 Console.WriteLine("Broj parnih brojeva u nizu je {0}", parni);
 Console.WriteLine("Broj neparnih brojeva u nizu je {0}", neparni);
