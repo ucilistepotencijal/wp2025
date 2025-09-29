9dd65b1ae8747c10e824d032af18c03a8fbfaf5b
Meri  Sumic <merisumic@gmail.com>
2025-09-29 19:26:01 +0200

    Ispravak zadatka 3

--- Predavanje9/KSuKW/Program.cs
+++ Predavanje9/KSuKW/Program.cs
@@ -6,7 +6,7 @@ while (true)
 {
 	try
 	{
-		Console.WriteLine("Upiši 'KS' za pretvorbu u 'KW' i obrnuto (ili 0 za izlaz): ");
+		Console.Write("Upiši 'KS' za pretvorbu u 'KW' i obrnuto (ili 0 za izlaz): ");
 		string unos = Console.ReadLine();
 
 		if (unos == "0") break;
@@ -14,16 +14,14 @@ while (true)
         if (unos.ToLower() == "ks")
         {
             Console.Write("Unesi vrijednost u KS: ");
-            decimal KS = decimal.Parse(Console.ReadLine());
-            decimal KW = KS * 0.736m;
-            Console.WriteLine("{0} KS = {1} KW", KS, KW);
+            int KS = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0} KS = {1} KW", KS, KSuKW(KS));
         }
         else if (unos.ToLower() == "kw")
         {
             Console.Write("Unesi vrijednost u KW: ");
-            decimal KW = decimal.Parse(Console.ReadLine());
-            decimal KS = KW * 1.359m;
-            Console.WriteLine("{0} KS = {1} KW", KW, KS);
+            int KW = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0} KW = {1} KS", KW, KWuKS(KW));
         }
         else
         {
@@ -35,4 +33,20 @@ while (true)
 
 		Console.WriteLine("Pogrešan unos, molim ponovite!");
 	}
+}
+
+partial class Program
+{
+    static int KSuKW(int KS)
+    {
+        {
+            return (int)Math.Round(KS * 0.736, 0); // ako ne roundamo prvo onda će samo odrezati decimale
+        }
+    }
+    static int KWuKS(int KW)
+    {
+        {
+            return (int)Math.Round(KW * 1.359, 0); // ako ne roundamo prvo onda će samo odrezati decimale
+        }
+    }
 }
