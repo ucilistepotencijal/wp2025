b3ff69f24013ccb6da295249a6b6a38f30962c1d
ucilistepotencijal <144696093+ucilistepotencijal@users.noreply.github.com>
2025-09-29 21:02:41 +0200

    Finished Predavanje10

---
+++ Predavanje10/MainTest/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace MainTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    var P = UnesiTocku("P");
+                    var R = UnesiTocku("R");
+
+                    if (P.x == 0 || P.y == 0 || R.x == 0 || R.y == 0)
+                    {
+                        Console.WriteLine("Unos ne može biti 0!");
+                        return;
+                    }
+
+                    double d = Math.Sqrt(Math.Pow(P.x - R.x, 2) + Math.Pow(P.y - R.y, 2));
+                    Console.WriteLine("Udaljenost između točaka: " + d);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Pogrešan unos, molim ponovite!");
+                }
+            }
+        }
+
+        static (double x, double y) UnesiTocku(string tocka)
+        {
+            Console.WriteLine($"Unesite koordinate točke {tocka}:");
+            Console.Write("Prva koordinata: ");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("Druga koordinata: ");
+            double y = double.Parse(Console.ReadLine());
+            return (x, y);
+        }
+    }
+}
---
+++ Predavanje10/Zadaci04_Prosjek/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+ Napiši program koji će na temelju unesene prosječne ocjene ispisati poruku o uspjehu
+ */
+bool bIspravno = false;
+double ocjena = -1;
+while (!bIspravno)
+{
+    Console.Write("Unesi prosječnu ocjenu: ");
+    try
+    {
+        ocjena = double.Parse(Console.ReadLine());
+        if (ocjena < 1 || ocjena > 5)
+        {
+            throw new Exception("Variable out of range!");
+        }
+        bIspravno = true;   
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Greška: " + ex.Message);  
+    }
+}
+
+Console.WriteLine("Uspjeh je {0}.", Prosjek(ocjena));
+
+partial class Program
+{
+    static string Prosjek(double prosjek)
+    {
+        if (prosjek >= 4.5)
+        {
+            return "odličan";
+        }
+        else if (prosjek >= 3.5)
+        {
+            return "vrlo dobar";
+        }
+        else if (prosjek >= 2.5)
+        {
+            return "dobar";
+        }
+        else if (prosjek >= 1.5)
+        {
+            return "dovoljan";
+        }
+        else
+        {
+            return "nedovoljan";
+        }
+    }
+}
