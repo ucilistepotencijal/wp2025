a134eb9e657b6b668744c7066463654eb637467c
Meri  Sumic <merisumic@gmail.com>
2025-09-29 21:00:51 +0200

    Finished Predavanje10

---
+++ Predavanje10/ProstBroj/Program.cs
@@ -0,0 +1,70 @@
+﻿/* Napiši program u kojem se u funkciji ispituje je li uneseni prirodan broj prost ili složen i
+odgovor ispisuje na zaslon. U programu korisnik treba imati mogućnost ponovnog računanja
+ako unutar glavne metode na pitanje „Želiš li ponovo (D/N)?“ odgovori potvrdno.
+Napomena: Broj je prost ako je djeljiv samo sa 1 i sa samim sobom */
+
+using System.Collections;
+
+bool bPonovi = true;
+
+while (bPonovi)
+{
+    try
+    {
+        Console.Write("Unesi prirodni broj: ");
+        int iBroj = int.Parse(Console.ReadLine());
+        if (iBroj < 0)
+        {
+            throw new Exception("Niste unijeli prirodan broj!");
+        }
+        string sProstSlozen = Prost(iBroj) ? "prost" : "složen";
+
+        // isto kao:
+        //string sProstSlozen = "";
+        //if (Prost(iBroj) == true)
+        //{
+        //  sProstSlozen = "Prost";
+        //}
+        //else
+        //{
+        //  sProstSlozen = "Složen";
+        //}
+
+
+        Console.Write("Broj je {0}. ", sProstSlozen);
+
+        Console.Write("Želiš li ponovo (D/N)?");
+        string unos = Console.ReadLine();
+        if (unos.ToLower() == "n")
+        {
+            bPonovi = false;
+        }
+    }
+    catch (Exception e)
+    {
+
+        Console.WriteLine("Dogodila se greška: " + e);
+    }
+}
+partial class Program
+{
+    static bool Prost(int broj)
+    {
+        int brojDjelitelja = 0;
+        for (int i = 1; i <= broj; i++)
+        {
+            if (broj % i == 0)
+            {
+                brojDjelitelja++;
+            }
+        }
+        if (brojDjelitelja == 1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
---
+++ Predavanje10/Zadaci04_Prosjek/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Napiši program koji će u funkciji na temelju unesene prosječne ocjene ispisaƟ poruku o
+uspjehu.
+Primjer: Za unesenu prosječnu ocjenu 3,4 program ispisuje „Prosjek je dobar“. */
+
+using System.Collections;
+
+Console.Write("Unesi prosječnu ocjenu: ");
+double ocjena = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Uspjeh je {0}.", Prosjek(ocjena));
+
+partial class Program
+{
+    static string Prosjek(double prosjek)
+    {
+        if (prosjek >= 4.5)
+        {
+            return "odličan";
+        }
+        else if (prosjek >= 3.5)
+        {
+            return "vrlo dobar";
+        }
+        else if (prosjek >= 2.5)
+        {
+            return "dobar";
+
+        }
+        else if (prosjek >= 1.5)
+        {
+            return "dovoljan";
+        }
+        else
+        {
+            return "nedovoljan";       
+        }
+    }
+}
