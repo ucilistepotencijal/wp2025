ffb815066229bb73db4a5ec97242e8b0ad7958e0
franjobalog-95 <franjo.balog@gmail.com>
2025-09-29 17:49:08 +0200

    Added zadatak 05

---
+++ Predavanje08/Zadatak05Znamenke/Program.cs
@@ -0,0 +1,21 @@
+﻿// Unesi prirodni broj, a zatim znamenke upisanog broja ispisuj unatrag. 
+// Svaku znamenku broja spremiti kao poseban element kolekcije
+using System.Collections;
+
+ArrayList listaZnamenki = new ArrayList();
+
+Console.Write("Upiši višeznamenkasti Prirodni broj: ");
+int broj = int.Parse(Console.ReadLine());
+
+while (broj > 0)
+{
+    int znamenka = broj % 10;
+    broj /= 10;
+    listaZnamenki.Add(znamenka);
+}
+        
+Console.WriteLine("Ispis višeznamenkastog prirodnog broja unazad: ");
+for (int i = 0;i < listaZnamenki.Count; i++)
+{
+    Console.WriteLine(listaZnamenki[i]);
+}
