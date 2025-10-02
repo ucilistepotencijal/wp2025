f1895eeaf6d2968670ed9f14cd1c6c6cf1ecb518
franjobalog-95 <franjo.balog@gmail.com>
2025-10-02 16:57:00 +0200

    Added dovršen palindrom

--- Predavanje11/Palindrom/Program.cs
+++ Predavanje11/Palindrom/Program.cs
@@ -1,13 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Unesi rečenicu ili riječ: ");
+string recenica = Console.ReadLine();
+if (Palindrom(recenica))
+{
+    Console.WriteLine("Riječ ili rečenica je palindrom!");
+}
+else
+{
+    Console.WriteLine("Riječ ili rečenica nije palindrom!");
+}
 
-
-partial class program
+partial class Program
 {
     static bool Palindrom(string recenica)
     {
         recenica = recenica.Replace(" ", "").Replace(",", "").Replace(".", "").Replace("!", "").Replace("?", "").Trim().ToLower();
         string novaRecenica = "";
-
+        foreach (char item in recenica.Reverse())
+        {
+            novaRecenica += item;
+        }
+        bool jestPalindrom = novaRecenica == recenica;
+        return jestPalindrom;
     }
 }
