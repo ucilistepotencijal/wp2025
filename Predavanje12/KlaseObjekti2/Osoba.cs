3bb0784b43dacc2f1676a7f0a69b1d3cede2e88b
ucilistepotencijal <144696093+ucilistepotencijal@users.noreply.github.com>
2025-10-06 21:00:26 +0200

    Finished Predavanje12

---
+++ Predavanje12/KlaseObjekti2/Osoba.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlaseObjekti2
+{
+    internal class Osoba
+    {   //Static članovi odnose se na cijelu klasu
+        //Private modifikator pristupa označava privatne članove klase
+        private static int brojOsoba = 0;
+        public static int DohvatiBrojOsoba()
+        {
+            return brojOsoba;
+        }
+
+        //konstruktor klase Osoba
+        public Osoba()
+        {
+            brojOsoba++;
+        }
+
+        public Osoba (string ime, string prezime)
+        {
+            Ime = ime;
+            Prezime = prezime;
+        }
+
+
+
+         //Modifikator pristupa public omogućuje pristupanje svojstvima, metodama i događajima izvan definicije klase
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+
+        //Definicija metode (funkcije)
+        public string PunoIme()
+        {
+            return Ime + " " + Prezime;
+        }
+    }
+}
