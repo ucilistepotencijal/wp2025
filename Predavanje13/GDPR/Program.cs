ac4cf8706c7feca0ee31fd5b0eeb60296e96263b
Meri  Sumic <merisumic@gmail.com>
2025-10-07 21:03:20 +0200

    Finished Predavanje13 (dovršiti zadatak)

---
+++ Predavanje13/GDPR/Program.cs
@@ -0,0 +1,34 @@
+﻿using GDPR;
+
+List<Student> studenti = new List<Student>();
+
+while (true)
+{
+	Student s = new Student();
+	Console.Write("Unesi ime studenta (ili kraj za prekid unosa): ");
+	s.Ime = Console.ReadLine();
+	studenti.Add(s);
+
+	if (s.Ime.ToLower() == "kraj") break;
+
+	Console.Write("Unesi prezime studenta: ");
+	s.Prezime = Console.ReadLine();
+
+	Console.Write("Unesi godinu rođenja (bez točke na kraju): ");
+	s.GodinaRodenja = int.Parse(Console.ReadLine());
+
+	Console.Write("Unesi mjesto rodenja: ");
+	s.MjestoRodenja = Console.ReadLine();
+
+	Console.Write("Unesi OIB: ");
+	s.OIB = Console.ReadLine();
+
+    Console.WriteLine("--------------- Popis studenata -------------");
+	foreach (Student x in studenti)
+	{
+        Console.WriteLine($"Inicijali: {s.Inicijali}");
+        Console.WriteLine($"Starost: {s.Starost()} godina");
+        Console.WriteLine($"OIB je {(s.ProvjeriOIB() ? "ispravan" : "neispravan")}");
+        Console.WriteLine("\n");
+    }
+}
---
+++ Predavanje13/GDPR/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDPR
+{
+    internal class Student
+    {
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public int GodinaRodenja { get; set; }
+        public string MjestoRodenja { get; set; }
+        public string OIB { get; set; }
+        public string Inicijali()
+        {
+            return $"{Ime[0]}.{Prezime[0]}.";
+        }
+        public int Starost()
+        {
+            return DateTime.Now.Year - GodinaRodenja;
+        }
+        public bool ProvjeriOIB()
+        {
+            if (OIB.Length == 11)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
