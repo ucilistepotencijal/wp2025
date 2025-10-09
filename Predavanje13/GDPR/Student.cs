cd52d39ab0a7740069c86fc3b95958be32165fe6
valeriomatovina-tech <valeriomatovina@gmail.com>
2025-10-09 21:23:05 +0200

    Predavanje

--- Predavanje13/GDPR/Student.cs
+++ Predavanje13/GDPR/Student.cs
@@ -8,5 +8,43 @@ namespace GDPR
 {
     internal class Student
     {
+        public string Ime { get; set; }
+
+        public string Prezime { get; set; }
+        public string GodinaRođenja { get; set; }
+        public string MjestoStudiranja { get; set; }
+        public string OIB { get; set; }
+
+        public string Inicijali { get; set; }
+
+        {
+        return $" {Ime[0]}.{Prezime[0]}."}
+
+    public int Starost()
+    return DataTime.Now.Year - godinaRođenja;
+        public bool ProvjeriOIB()
+    {
+        if (OIB.Lenght != 11 || !ulong Tryparse(OIB, out ulong iOB))
+    {
+            return false;
+        }
+        int iOstatak = 10;
+
     }
+        for(int i = 0; i<OIB.Lenght -1;  i++) 
+        {
+            int iZnamenka = OIB[i];
+    iOstatak = iZnamenka + iOstatak;
+              
+             iOstatak = iOstatak % 10;
+        if (iOstatak == 0)
+        {
+        iOstatak = 10;
+        }
+
+
+        int iCjelobrojnoPodijelis10 = iZbrojS 10 % 10;
+        int 
+}
+    
 }
--- SamostalnoVjezbanje/PrviZadatak/Program.cs
+++ SamostalnoVjezbanje/PrviZadatak/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Upisi ime: ");
+string ime = Console.ReadLine();
+if ("a"=="a")
+{
+
+}
+List<char> lista = new List<char>();
+foreach (char c in lista)
+{
+    Console.WriteLine("Svako c u listi je :");
+}
