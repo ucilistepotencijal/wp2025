24135ceddf2b31209674a73123d650993437cd1f
ucilistepotencijal <144696093+ucilistepotencijal@users.noreply.github.com>
2025-10-09 21:04:44 +0200

    Finished Predavanje14

---
+++ Predavanje14/Nasljedjivanje/Clan.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nasljedjivanje
+{
+    internal class Clan : Osoba
+    {
+        //konstruktor nije moguće naslijediti, nego moramo napraviti svoj konstruktor
+        public Clan(string ime, string prezime) : base(ime, prezime)
+        {
+        }
+
+        public string ClanskiBroj { get; set; }
+
+        public string Detalji()
+        {
+            return PunoIme() + ", id: " + ClanskiBroj;
+        }
+
+        //skrivanje metode (hide)
+        public new string PunoIme()
+        {
+            return Prezime + " " + Ime;
+        }
+
+        public override string ToString()
+        {
+            return Detalji();
+        }
+    }
+}
---
+++ Predavanje14/RefAndOut/Program.cs
@@ -0,0 +1,54 @@
+﻿using RefAndOut;
+
+Console.Write("Unesi broj: ");
+int broj = int.Parse(Console.ReadLine());
+
+//ispisujem broj
+Ispis(broj);
+
+//Pozivamo metodu kojoj prosljeđujemo broj po vrijednosti
+Inkrementiraj(broj);
+
+//Ispisujem broj
+Ispis(broj);
+
+//Pozivamo metodu kojoj prosljeđujemo broj po vrijednosti
+InkrementirajPoReferenci(ref broj);
+
+//Ispisujem broj
+Ispis(broj);
+
+Osoba o = new Osoba();
+o.Ime = "Ivo";
+//o = null;
+PromijeniIme(o); // Objekti se po defaultu prosljeđuju po referenci
+Console.WriteLine(o.Ime);
+o = null;
+PromijeniPrezime(out o);
+Console.WriteLine(o.Prezime);
+
+partial class Program
+{
+    static void Inkrementiraj(int broj)
+    {
+        broj++;
+    }
+    static void Ispis(int broj)
+    {
+        Console.WriteLine("Vrijednost broja je {0}", broj);
+    }
+    static void InkrementirajPoReferenci(ref int broj)
+    {
+        broj++;
+    }
+
+    static void PromijeniIme(Osoba osoba)
+    {
+        osoba.Ime = "Pero";
+    }
+    static void PromijeniPrezime(out Osoba osoba)
+    {
+        osoba = new Osoba();
+        osoba.Prezime = "Perić";
+    }
+}
