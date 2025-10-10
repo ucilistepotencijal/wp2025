fccfec912579a11ccf1cb22a1f5a117cbe7e6a85
ucilistepotencijal <144696093+ucilistepotencijal@users.noreply.github.com>
2025-10-10 21:13:15 +0200

    Finished predavanje 15

---
+++ Predavanje15/Vozila_DAL/Automobil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vozila_DAL
+{
+    public class Automobil: Vozilo
+    {
+        public int Ccm { get; set; }
+        public override string ToString()
+        {
+            return $"Naziv: {Naziv}, KS: {KS}";
+        }
+    }
+}
