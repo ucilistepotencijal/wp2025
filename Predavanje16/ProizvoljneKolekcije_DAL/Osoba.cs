58af2b36e341286ea762f93eccba42b7e821e148
franjobalog-95 <franjo.balog@gmail.com>
2025-10-13 20:47:00 +0200

    Added Predavanje16

---
+++ Predavanje16/ProizvoljneKolekcije_DAL/Osoba.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProizvoljneKolekcije_DAL
+{
+    public class Osoba
+    {
+        #region Svojstva
+        private string ime;
+        private string prezime;
+        public string Ime { get => ime; set => ime = value; }
+        public string Prezime { get => prezime; set => prezime = value; }
+        #endregion
+
+        #region Metode
+        public string PunoIme()
+        { 
+            return Ime + " " + Prezime;
+        }
+        public override string ToString()
+        {
+            return PunoIme();
+        }
+    
+        #endregion
+    }
+}
---
+++ Predavanje16/ProizvoljneKolekcije_DAL/Polaznici.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProizvoljneKolekcije_DAL
+{
+    public class Polaznici: CollectionBase
+    {
+        public int Add(object polaznik)
+        {
+            return InnerList.Add(polaznik);
+        }
+        public int Add(string ime, string prezime)
+        {
+            Polaznik p = new Polaznik();
+            p.Ime = ime;
+            p.Prezime = prezime;
+            return InnerList.Add(p);
+        }
+
+        //Implementacija indexera
+        public object this[int index]
+        {
+            get
+            {
+                return (Polaznici)InnerList[index];
+            }
+        }
+    }
+}
