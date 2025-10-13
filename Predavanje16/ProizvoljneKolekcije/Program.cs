3124b5afca0df4fe4c2f6cd706aa5eb9806ec4a7
Meri  Sumic <merisumic@gmail.com>
2025-10-13 20:40:48 +0200

    Finished Predavanje16

---
+++ Predavanje16/ProizvoljneKolekcije/Program.cs
@@ -0,0 +1,45 @@
+﻿using ProizvoljneKolekcije_DAL;
+using System.Collections;
+
+Polaznik p1 = new Polaznik();
+p1.Ime = "Pero";
+p1.Prezime = "Perić";
+p1.ProgramiObrazovanja = "ASP.NET";
+
+Polaznik p2 = new Polaznik();
+p2.Ime = "Ana";
+p2.Prezime = "Anić";
+p2.ProgramiObrazovanja = "WORDPRESS";
+
+ArrayList polaznici = new ArrayList();
+polaznici.Add(p1);
+polaznici.Add(p2);
+
+Console.WriteLine("Foreach ispis...");
+
+foreach (Polaznik p in polaznici)
+{
+    Console.WriteLine(p.PunoIme());
+}
+
+Console.WriteLine("For ispis...");
+
+for (int i = 0; i < polaznici.Count; i++)
+{
+    Console.WriteLine(((Polaznik)polaznici[i]).PunoIme());
+}
+
+Console.WriteLine();
+Console.WriteLine("Implementacija pomoću proizvoljne kolekcije");
+
+//2. Implementacija pomoću proizvoljne kolekcije
+Polaznici polazniciKolekcija = new Polaznici();
+polazniciKolekcija.Add(p1);
+polazniciKolekcija.Add(p2);
+polazniciKolekcija.Add("Ivan", "Ivanović");
+
+foreach (Polaznik p in polazniciKolekcija)
+{
+    Console.WriteLine(p.PunoIme());
+}
+
---
+++ Predavanje16/ProizvoljneKolekcije_DAL/Polaznik.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProizvoljneKolekcije_DAL
+{
+    public class Polaznik: Osoba
+    {
+        public string ProgramiObrazovanja { get; set; }
+        public string PunoIme()
+        {
+            return Prezime + " " + Ime;
+        }
+        public override string ToString()
+        {
+            return PunoIme() + ", " + ProgramiObrazovanja;
+        }
+    }
+}
