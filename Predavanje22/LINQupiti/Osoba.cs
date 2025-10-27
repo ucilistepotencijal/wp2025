9b5e9e3cce0a231a4074a5b35f549a1f2ce68c86
Meri  Sumic <merisumic@gmail.com>
2025-10-27 21:01:12 +0100

    Finished Predavanje22

---
+++ Predavanje22/LINQupiti/Osoba.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQupiti
+{
+    internal class Osoba
+    {
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+
+    }
+}
---
+++ Predavanje22/LINQupiti/Program.cs
@@ -0,0 +1,27 @@
+﻿using LINQupiti;
+
+List<Osoba> osobe = new List<Osoba>();
+osobe.Add(new Osoba { Ime = "Pero", Prezime = "Perić" });
+osobe.Add(new Osoba { Ime = "Ana", Prezime = "Anić" });
+osobe.Add(new Osoba { Ime = "Ivo", Prezime = "Ivić" });
+osobe.Add(new Osoba { Ime = "Marko", Prezime = "Marković" });
+
+Osoba trazenaOsoba = new Osoba();
+foreach (Osoba o in osobe)
+{
+	if (o.Ime == "Marko")
+	{
+		trazenaOsoba = o;
+        Console.WriteLine("{0} {1}", o.Ime, o.Prezime);
+	}
+}
+
+//Pretraga sa LINQ upitima
+Osoba trazenaOsoba2 = (from o in osobe
+					   where o.Ime == "Marko"
+					   select o).SingleOrDefault();
+if(trazenaOsoba2 != null)
+{
+    Console.WriteLine("{0} {1}", trazenaOsoba2.Ime, trazenaOsoba2.Prezime);
+}
+
