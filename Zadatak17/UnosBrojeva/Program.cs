7f5c9dd74d6d8674e85e78962f3c1108aa393c01
valeriomatovina-tech <valeriomatovina@gmail.com>
2025-10-29 21:09:56 +0100

    Finish zadatak 17

---
+++ Zadatak17/UnosBrojeva/Program.cs
@@ -0,0 +1,39 @@
+﻿
+        List<int> brojevi = new List<int>();
+int broj = -1;
+
+        Console.Write("Unesite brojeve (0 za kraj): ");
+        while (true)
+        {
+            Console.Write("Unesi broj: ");
+            try
+            {
+
+            }
+            broj = int.Parse(Console.ReadLine());
+            if (broj == 0)
+            {
+                break;
+            }
+            else
+            {
+                brojevi.Add(broj);
+            }
+
+            IntervalBrojevi(brojevi);
+        }
+        catch(Exeption e)
+        {
+
+
+
+        }
+    public static void IntervalBrojevi(List<int> brojevi)
+    {
+        var intervalBrojevi = brojevi.Where(b => b >= 1 && b <= 11);
+        foreach (int item in intervalBrojevi)
+        {
+            Console.WriteLine(item + ", ");
+        }
+
+    }
