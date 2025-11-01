e0438777f8165ca2d0d33843205267e4deef3d59
Meri  Sumic <merisumic@gmail.com>
2025-11-01 12:21:31 +0100

    1. zadatak

---
+++ ProvjeraZnanja2/Zadatak01/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Zadatak 1: Napravi program koji traži unos teksta, a zatim ispisuje taj tekst
+„naopako”. 1 bod
+ Npr. za uneseni tekst „Danas je lijep dan” treba ispisati „nad pejil ej sanaD” */
+
+string recenica = "";
+while(recenica.Length < 1)
+{
+	Console.Write("Unesi rečenicu: ");
+	recenica = Console.ReadLine().Trim('.', ',', '?', '!');
+	if (recenica.Length == 0)
+	{
+		Console.WriteLine("Unos ne smije biti prazan. Molimo ponovite unos.");
+		recenica = "";
+	}
+}
+
+string recenica1 = new string (recenica.Reverse().ToArray());
+foreach (char c in recenica1)
+{
+    Console.Write(c);
+}
