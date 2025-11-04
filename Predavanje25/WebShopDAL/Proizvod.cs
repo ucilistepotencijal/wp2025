a3c2554b3630060cf77a2a023428b0de63270777
ucilistepotencijal <144696093+ucilistepotencijal@users.noreply.github.com>
2025-11-04 21:02:45 +0100

    Added class Proizvod

--- Predavanje25/WebShopDAL/Proizvod.cs
+++ Predavanje25/WebShopDAL/Proizvod.cs
@@ -8,5 +8,20 @@ namespace WebShopDAL
 {
     public class Proizvod
     {
+        public string Naziv { get; set; }
+        public string Opis { get; set; }
+        public double JedCijena { get; set; }
+        public bool Dostupan { get; set; }
+
+        public double UkupnaCijena
+        {
+            get { return IzracunajPDV(JedCijena); }
+        }
+
+        public double IzracunajPDV(double cijena)
+        {
+            return cijena * 1.25;
+        }       
+
     }
 }
