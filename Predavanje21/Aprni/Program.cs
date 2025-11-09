8ec17b409144e0a4598fd5764f9d8fe05e341c7f
Meri  Sumic <merisumic@gmail.com>
2025-11-09 09:19:26 +0100

    Kopiran folder Predavanje21 s main brancha

---
+++ Predavanje21/Aprni/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ Napiši program koji sve parne brojeve iz intervala [1, 100] zapisuje u datoteku Parni.txt i zatvara
+ ju. Nakon toga otvara tu istu datoteku i njezin sadržaj ispisuje na ekran,
+ */
+
+string datoteka = @"D:\Parni.txt";
+
+
+FileStream fs = new FileStream(datoteka, FileMode.Create);
+StreamWriter sw = new StreamWriter(fs);
+
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 2 == 0)
+        sw.WriteLine(i);
+}
+
+//sw.Flush();
+sw.Close();
+//fs.Close();
+
+FileStream fs2 = new FileStream(datoteka, FileMode.Open);
+StreamReader sr = new StreamReader(fs2);
+
+string sadrzaj = sr.ReadToEnd();
+sr.Close();
+//fs2.Close();
+
+Console.WriteLine("sadrzaj datoteke Parni.txt:\n");
+Console.WriteLine(sadrzaj);
