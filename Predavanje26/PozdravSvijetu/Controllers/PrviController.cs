b9028257e92a487271d5b4ddea17ed7b34c81781
Meri  Sumic <merisumic@gmail.com>
2025-11-27 12:13:09 +0100

    Dodani Predavanje26 i Predavanje27 iz main

---
+++ Predavanje26/PozdravSvijetu/Controllers/PrviController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PozdravSvijetu.Controllers
+{
+    public class PrviController : Controller
+    {
+        public string Pozdrav()
+        {
+            return "Pozdrav s prvog kontrolera.";
+        }
+
+        public IActionResult ImeStroja()
+        {
+            string imeStroja = "<h2>" + Environment.MachineName + "</h2>";
+            return Content(imeStroja, "text/html");
+        }
+
+        public string MetodaSaParametrima(string naziv)
+        {
+            return "Pozdrav, " + naziv + "!";
+        }
+
+        public string MetodaSaParametrima2(int? id)
+        {
+            return "Pozdrav, " + id + "!";
+        }
+    }
+}
