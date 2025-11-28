7347ce3661d715ef34f34afe772f192166e492fd
Igor <Igor@LAPTOP-9MMVF3SF>
2025-11-28 20:52:17 +0100

    Finsihed Predavanje 28

---
+++ Predavanje28/Modeli/Controllers/SimpleBindingController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Modeli.Controllers
+{
+    public class SimpleBindingController : Controller
+    {
+        public IActionResult SimpleBindMetoda()
+        {
+            return View("SimpleBind");
+        }
+
+        [HttpPost]
+        public IActionResult SimpleBindMetoda(string ime)
+        {
+            string pozdravnaPoruka = $"Dobrodošli, {ime}!";
+            return View("SimpleBind", pozdravnaPoruka);
+        }
+    }
+}
