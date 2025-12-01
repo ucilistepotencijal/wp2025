5fcbec2e8eb7f07610ab15140315eb1228f0ea0a
Meri  Sumic <merisumic@gmail.com>
2025-12-01 11:12:23 +0100

    Kopiran folder iz maina Predavanje28

---
+++ Predavanje28/Modeli/Controllers/SimpleBindingController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Modeli.Controllers
+{
+    public class SimpleBindingController : Controller
+    {
+        public IActionResult SimpleBindMetoda()
+        {
+            return View("SimpleBind");
+        }
+
+        [HttpGet]
+
+        public IActionResult SimpleBindMetoda(string ime)
+        {
+            string pozdravPoruka = $"Dobrodošli, {ime}!";
+            return View("SimpleBind", pozdravPoruka);
+        }
+    }
+}
