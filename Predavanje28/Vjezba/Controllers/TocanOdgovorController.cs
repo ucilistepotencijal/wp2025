d5f2a64c98afa6b50b8e757c2aa0b55d00e0c651
Meri  Sumic <merisumic@gmail.com>
2025-12-09 17:14:40 +0100

    Kopirani folderi 25-28 iz main

---
+++ Predavanje28/Vjezba/Controllers/TocanOdgovorController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vjezba.Controllers
+{
+    public class TocanOdgovorController : Controller
+    {
+        public IActionResult ProvjeriOdgovor()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ProvjeriOdgovor(string odgovor)
+        {
+            if (odgovor == "8")
+            {
+                ViewBag.Poruka = "Točno";
+            }
+            else
+            {
+                ViewBag.Poruka = "Netočno";
+            }
+            return View();
+        }
+    }
+}
