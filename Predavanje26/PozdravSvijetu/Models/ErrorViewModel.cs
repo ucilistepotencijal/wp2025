1c79af3659ca12452d6c9e8440da5a6776c3d29a
Meri  Sumic <merisumic@gmail.com>
2025-12-09 16:51:38 +0100

    Deleted Predavanje25–Predavanje31

--- Predavanje26/PozdravSvijetu/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PozdravSvijetu.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Predavanje28/Modeli/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Modeli.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
