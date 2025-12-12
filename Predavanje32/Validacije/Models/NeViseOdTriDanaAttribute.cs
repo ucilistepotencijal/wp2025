19ecfec5c8554633b7ae3111f1012f9fd2a7dfca
Meri  Sumic <merisumic@gmail.com>
2025-12-12 16:25:54 +0100

    Dodano

---
+++ Predavanje32/Validacije/Models/NeViseOdTriDanaAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Validacije.Models
+{
+    public class NeViseOdTriDanaAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value is MetaRacun)
+            {
+                MetaRacun racun = (MetaRacun)value;
+                if (racun.DatumRacuna < DateTime.Now.AddDays(-3))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
