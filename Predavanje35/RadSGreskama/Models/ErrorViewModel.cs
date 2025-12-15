f95a9b5c60b4ee9b96cd9a7e9d1859dc01185792
Meri  Sumic <merisumic@gmail.com>
2025-12-15 18:23:27 +0100

    save

---
+++ Predavanje35/RadSGreskama/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RadSGreskama.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
