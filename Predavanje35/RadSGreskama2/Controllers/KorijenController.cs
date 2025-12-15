c03ef33bca8ee2d83d8904a6c61d16fa18b8239e
ucilistepotencijal <144696093+ucilistepotencijal@users.noreply.github.com>
2025-12-15 20:55:22 +0100

    Finished predavanje35

---
+++ Predavanje35/RadSGreskama2/Controllers/KorijenController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RadSGreskama2.Controllers
+{
+    public class KorijenController : Controller
+    {
+        public IActionResult Korijen()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Korijen(double broj)
+        {
+            try
+            {               
+                decimal rezultat = (decimal)Math.Sqrt(broj);
+                ViewBag.Rezultat = rezultat;
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Greska = "Došlo je do greške prilikom izračunavanja korijena: " + ex.Message;
+            }
+            return View();
+        }
+    }
+}
---
+++ Predavanje35/RadSGreskama2/Controllers/ZbrojController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RadSGreskama2.Models;
+
+namespace RadSGreskama2.Controllers
+{
+    public class ZbrojController : Controller
+    {
+        public IActionResult Zbroj()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Zbroj(decimal brojA, decimal brojB)
+        {
+            if (brojA == null || brojB == null || !ModelState.IsValid)
+            {
+                return View("Error", new ErrorViewModel());
+            }
+            decimal rezultat = brojA + brojB;
+            ViewBag.Rezultat = rezultat;
+
+            return View();
+        }
+    }
+}
