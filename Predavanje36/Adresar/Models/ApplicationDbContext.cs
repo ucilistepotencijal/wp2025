57c3c8961cb752104cd03c32ba35aed9c695be36
Igor <Igor@LAPTOP-9MMVF3SF>
2025-12-17 20:20:47 +0100

    Added Adresar App

--- Predavanje36/Adresar/Models/ApplicationDbContext.cs
+++ Predavanje36/Adresar/Models/ApplicationDbContext.cs
@@ -4,5 +4,9 @@ namespace Adresar.Models
 {
     public class ApplicationDbContext: DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Kontakt> Kontakti { get; set; }
     }
 }
