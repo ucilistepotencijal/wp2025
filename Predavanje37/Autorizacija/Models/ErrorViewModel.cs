2e05e6c101bce477b6b5ee0a57a1801db157aca6
Igor <Igor@LAPTOP-9MMVF3SF>
2025-12-19 19:10:36 +0100

    Added Predavanje36

---
+++ Predavanje37/Autorizacija/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Autorizacija.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
