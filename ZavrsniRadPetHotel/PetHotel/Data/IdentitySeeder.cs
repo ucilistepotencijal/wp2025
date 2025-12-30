45cd59cf3c327bede5160c891b8f2b8a79a31314
Meri  Sumic <merisumic@gmail.com>
2025-12-30 11:26:01 +0100

    Program.cs, Layout and LoginPartial changed

---
+++ ZavrsniRadPetHotel/PetHotel/Data/IdentitySeeder.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace PetHotel.Data // Pazi da je namespace isti kao u ApplicationDbContext
+{
+    public static class IdentitySeeder
+    {
+        public static async Task SeedRolesAsync(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+            // 1. Stvaranje uloga (Admin i User)
+            string[] roleNames = { "Admin", "User" };
+            foreach (var roleName in roleNames)
+            {
+                if (!await roleManager.RoleExistsAsync(roleName))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+
+            // 2. Stvaranje prvog Admin korisnika
+            var adminEmail = "admin@pethotel.com";
+            var adminUser = await userManager.FindByEmailAsync(adminEmail);
+
+            if (adminUser == null)
+            {
+                var newAdmin = new IdentityUser
+                {
+                    UserName = adminEmail,
+                    Email = adminEmail,
+                    EmailConfirmed = true
+                };
+
+                // Postavljamo lozinku (promijeni je kasnije!)
+                var result = await userManager.CreateAsync(newAdmin, "Admin123!");
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(newAdmin, "Admin");
+                }
+            }
+        }
+    }
+}
--- ZavrsniRadPetHotel/PetHotel/Program.cs
+++ ZavrsniRadPetHotel/PetHotel/Program.cs
@@ -1,29 +1,89 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using PetHotel.Data; // Provjeri je li ovo tvoj to?an namespace (ime projekta.Data)
+using PetHotel.Models;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// --- 1. POSTAVKE BAZE PODATAKA ---
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+// --- 2. POSTAVKE KORISNIKA (IDENTITY) ---
+builder.Services.AddDefaultIdentity<IdentityUser>(options => {
+    options.SignIn.RequireConfirmedAccount = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequiredLength = 6;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+})
+.AddRoles<IdentityRole>() // Omogu?uje Admin ulogu
+.AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+// --- 3. LOKALIZACIJA (HRVATSKI JEZIK) ---
+var hrCulture = new CultureInfo("hr-HR");
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    options.DefaultRequestCulture = new RequestCulture(hrCulture);
+    options.SupportedCultures = new[] { hrCulture };
+    options.SupportedUICultures = new[] { hrCulture };
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+// --- 4. KONFIGURACIJA HTTP CJEVOVODA (MIDDLEWARE) ---
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
+// Postavljanje HR kulture za datume i brojeve
+CultureInfo.DefaultThreadCurrentCulture = hrCulture;
+CultureInfo.DefaultThreadCurrentUICulture = hrCulture;
+app.UseRequestLocalization();
+
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
 app.UseRouting();
 
+// VAŽNO: Autentifikacija mora biti prije autorizacije
+app.UseAuthentication();
 app.UseAuthorization();
-
-app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapRazorPages();
+
+// --- 5. SEEDANJE ULOGA (Stvaranje Admina) ---
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        await IdentitySeeder.SeedRolesAsync(services);
+    }
+    catch (Exception ex)
+    {
+        // Ako nemaš još IdentitySeeder.cs, privremeno zakomentiraj red iznad (stavi // ispred)
+    }
+}
 
 app.Run();
