0b529da5823347e96ba239678222fd9dbca5bb05
Meri  Sumic <merisumic@gmail.com>
2025-12-30 11:07:42 +0100

    Added ApplicationDbContext and install NuGet packets

---
+++ ZavrsniRadPetHotel/PetHotel/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PetHotel.Models; // Provjeri da se projekt stvarno zove PetHotel
+
+namespace PetHotel.Data
+{
+    // IdentityDbContext nam omogućuje da imamo i korisnike i naše tablice
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Pet> Pets { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<ServiceType> ServiceTypes { get; set; }
+    }
+}
